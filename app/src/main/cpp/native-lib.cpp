2c59019066f5bdcba22ca249ffeaa21ed9801097
Kendall Stewart <kstwrt@cs.washington.edu>
2018-03-07 11:49:54 -0800

    initial commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,37 @@
+#include <jni.h>
+#include <math.h>
+
+#define SAMPLING_RATE 48000
+
+class FMCWSweepGenerator {
+
+public:
+    FMCWSweepGenerator(int baseband_hz, int bandwidth_hz, int duration_millis) {
+        this->baseband_hz = (float)baseband_hz;
+        this->bandwidth_hz = (float)bandwidth_hz;
+        duration_secs = duration_millis / 1000.0f;
+        t = 0;
+    }
+
+    void generate(int num_samples, float *audio_data) {
+        for (int i = 0; i < num_samples; ++i) {
+            float sample = sinf(
+                    bandwidth_hz / duration_secs * (float) M_PI *
+                    powf(t + baseband_hz / bandwidth_hz * duration_secs, 2)
+            );
+            audio_data[i] = sample;
+            t += 1.0 / SAMPLING_RATE;
+            if (t >= duration_secs) {
+                t = 0;
+            }
+        }
+    }
+
+private:
+    float t;
+
+    float baseband_hz;
+    float bandwidth_hz;
+    float duration_secs;
+};
+
---
+++ app/src/main/java/edu/washington/cs/puddlejumper/MainActivity.java
@@ -0,0 +1,21 @@
+package edu.washington.cs.puddlejumper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
